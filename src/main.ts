7a27c6e7b5a623e844b88f895ccdf8d0b91b37d0
yureezz12 <153299380+yureezz12@users.noreply.github.com>
2024-10-01 11:09:09 -0400

    a

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ const app = express()
 app.get("/produtos", async(req,res)=>{
     //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
     //1 - Criar a conexão com o banco
+    
     try{
         const conection = await mysql.createConnection({
-            host:process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"banco1022a",
+            host:process.env.dbhost?process.env.dbhost: 'localhost',
+            user:process.env.dbuser?process.env.dbuser:'root',
+            password:process.env.dbpassword?process.env.dbpassword:'',
+            database:process.env.dbname?process.env.dbname:'banco1022a',
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         //2 - Realizar uma consulta na tabela
