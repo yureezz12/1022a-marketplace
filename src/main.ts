da1c640bfef41d46465abe1e99d6c720fdf25b44
yureezz12 <komaninha12@gmail.com>
2024-10-27 22:19:43 -0300

    j

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.get("/produtos", async(req,res)=>{
+app.get("/usuarios", async(req,res)=>{
     //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
     //1 - Criar a conexão com o banco
     try{
@@ -16,7 +16,28 @@ app.get("/produtos", async(req,res)=>{
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         //2 - Realizar uma consulta na tabela
-        const [result, fields] = await conection.query("SELECT * from produtos")
+        const [result, fields] = await conection.query("SELECT * from usuarios")
+        await conection.end()
+        //3 - Devolver os dados pra quem pediu
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Server ERROR")
+    }
+})
+
+app.get("/usuarios", async(req,res)=>{
+    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
+    //1 - Criar a conexão com o banco
+    try{
+        const conection = await mysql.createConnection({
+            host:process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022a",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        //2 - Realizar uma consulta na tabela
+        const [result, fields] = await conection.query("SELECT * from usuarios")
         await conection.end()
         //3 - Devolver os dados pra quem pediu
         res.send(result)
