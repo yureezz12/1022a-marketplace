f29453806cb6d90a81c1a079c0345de3c0559308
yureezz12 <153299380+yureezz12@users.noreply.github.com>
2024-12-10 10:36:26 -0400

    fim

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,31 @@ app.get("/produtos", async(req,res)=>{
         //3 - Devolver os dados pra quem pediu
         res.send(result)
     }catch(e){
+        console.log(e)
+        res.status(500).send("Server ERROR")
+    }
+})
+app.post("/produtos", async(req,res)=>{
+    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
+    //1 - Criar a conexão com o banco
+    try{
+        const conection = await mysql.createConnection({
+            host:process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022a",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        //2 - Realizar uma consulta na tabela
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result, fields] = 
+                    await conection.query("INSERT INTO produtos VALUE (?,?,?,?,?)",
+                             [id,nome,descricao,imagem,preco])
+        await conection.end()
+        //3 - Devolver os dados pra quem pediu
+        res.send(result)
+    }catch(e){
+        console.log(e)
         res.status(500).send("Server ERROR")
     }
 })
